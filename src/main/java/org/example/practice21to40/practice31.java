c20e8025f172fa6c72011f73f83c7a9ca91912db
许新矅 <1305891523@qq.com>
2021-07-08 18:15:06 +0800

    ...

--- src/main/java/org/example/practice21to40/practice31.java
+++ src/main/java/org/example/practice21to40/practice31.java
@@ -1,45 +1,58 @@
 package org.example.practice21to40;
 
+import java.util.Arrays;
+
 /**
  * @Author xuxinyao
  * @Description: TODO
  * @Date 2021/7/5/10:00
  */
 public class practice31 {
-    //33. 搜索旋转排序数组
+    //31. 下一个排列
     //
-    //整数数组 nums 按升序排列，数组中的值 互不相同 。
+    //实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
     //
-    //在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+    //如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
     //
-    //给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
-    //
-    //
-    //
+    //必须 原地 修改，只允许使用额外常数空间。
     //示例 1：
-    //
-    //输入：nums = [4,5,6,7,0,1,2], target = 0
-    //输出：4
+    //输入：nums = [1,2,3]
+    //输出：[1,3,2]
 
     /**
-     * [left, r, right]
-     *  取mid = (left + right)/2
-     *  若nums[mid] > nums[0]，r在mid左边
-     *  若nums[mid] < nums[0]，r在mid右边
-     *  若target > nums[length - 1] ， 说明在[0 , r]
-     *  若target < nums[length - 1] ， 说明在[r, length - 1]
-     *  二分法
-     *  时间复杂度：O(log n)
+     * todo 字典序排序，加深理解
+     * @注意 ： 字典序的下一个排列，注意字典序的定义
+     * @思路：
+     *  1. 双指针
+     *  2. fast从后往前遍历，记录遇到的最大的值
+     *  3. 当发现后面有比当前值大的值时，slow指针开始从后往前遍历至fast，找到比当前值大的最小值，互换
+     *  4.@Warning 互换后要对较大值之后的序列进行升序排序，以保证是下一个排列
      */
-    public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length;
-        while (start < end){
-            int mid = (start + end) / 2;
-            if (nums[mid] > nums[start]) {
-
+    public void nextPermutation(int[] nums) {
+        int maxPos = nums.length - 1;
+        boolean flag = false;
+        for (int i = nums.length - 2; i >=0; i--) {
+            if (nums[i] >= nums[maxPos]) {
+                maxPos = i;
+            } else {
+                int  minBiggerPos = maxPos;
+                //slow开始移动
+                for (int j = nums.length - 1; j > i ; j--) {
+                    if (nums[j] > nums[i] && nums[j] <= nums[minBiggerPos]) {
+                        minBiggerPos = j;
+                        flag = true;
+                    }
+                }
+                if (flag) {
+                    int temp = nums[i];
+                    nums[i] = nums[minBiggerPos];
+                    nums[minBiggerPos] = temp;
+                }
+                break;
             }
         }
-        return -1;
+        if (!flag) {
+            Arrays.sort(nums);
+        }
     }
 }
---
+++ src/main/java/org/example/practice81to100/practice95.java
@@ -0,0 +1,33 @@
+package org.example.practice81to100;
+
+import org.example.dataStructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author xuxinyao
+ * @Description: TODO
+ * @Date 2021/7/7/16:11
+ */
+public class practice95 {
+    //95. 不同的二叉搜索树 II
+    //
+    //给你一个整数 n ，请你生成并返回所有由 n 个节点组成且节点值从 1 到 n 互不相同的不同 二叉搜索树 。可以按 任意顺序 返回答案。
+    //
+    //
+    //
+    //示例 1：
+    //
+    //输入：n = 3
+    //输出：[[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+    /**
+     * @思路：
+     *  1. 遍历n个数字，
+     *  2.
+     */
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> treeNodeList = new ArrayList<>();
+    }
+}
