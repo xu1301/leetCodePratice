764bb6a6e81b2f9632875f3164be77e78a3171eb
xu1301 <1305891523@qq.com>
2021-08-15 23:52:47 +0800

    构造二叉搜索树

---
+++ src/main/java/org/example/treePractice/ConstructBinaryTreePreAndIn.java
@@ -0,0 +1,22 @@
+package org.example.treePractice;
+
+import org.example.dataStructure.TreeNode;
+
+/**
+ * @author xuxinyao
+ * @since 2021/8/15/23:40
+ */
+public class ConstructBinaryTreePreAndIn {
+    /**
+     * @see <a href="https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/">从前序与中序遍历序列构造二叉树</a>
+     * @param preorder  前序遍历序列
+     * @param inorder   中序遍历序列
+     * @return
+     */
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+
+    }
+
+    public
+}
