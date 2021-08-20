c8772ac5c102246b49421ab04433f952bd948a7d
许新矅 <1305891523@qq.com>
2021-08-20 19:19:14 +0800

    some homework about binaryTree

---
+++ src/main/java/org/example/treePractice/BalancedBinaryTree.java
@@ -0,0 +1,19 @@
+package org.example.treePractice;
+
+import org.example.dataStructure.TreeNode;
+
+/**
+ * @author xuxinyao
+ * @since 2021/8/20/10:28
+ */
+public class BalancedBinaryTree {
+    /**
+     * <a href="https://leetcode-cn.com/problems/balanced-binary-tree/">平衡二叉树</a>
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+
+    }
+
+}
