1d81d7f71b8a0e17c23a165be9bbb363f33ff467
许新矅 <1305891523@qq.com>
2021-08-21 16:26:51 +0800

    some homework about binaryTree

---
+++ src/main/java/org/example/treePractice/DiameterOfBinaryTree.java
@@ -0,0 +1,18 @@
+package org.example.treePractice;
+
+import org.example.dataStructure.TreeNode;
+
+/**
+ * @author xuxinyao
+ * @since 2021/8/21/9:25
+ */
+public class DiameterOfBinaryTree {
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+
+    }
+}
